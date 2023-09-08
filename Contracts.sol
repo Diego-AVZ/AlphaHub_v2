68a5c7ad8c742daa8461163f8f04dc1d09b95429
Diego-AVZ <124291993+Diego-AVZ@users.noreply.github.com>
2023-09-08 22:10:07 +0200

    Contracts.sol

--- Contracts.sol
+++ Contracts.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT;
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -8,6 +8,87 @@ contract AlphaProviderAccess is ERC721{
     address owner;
 
     constructor () ERC721("AlphaProvider", "AlphaProv") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner () {
+        require(msg.sender == owner, "Not Owner");
+        _;
+    }
+
+    bool public isAlphaProv;
+    uint public price;
+    uint public supply;
+    mapping(address => uint) public balance; 
+    address public yourUrl;
+    mapping(address => address) public ZonasProvs;
+    uint public balanceETH;
+
+
+    function setPrice(uint _price) public onlyOwner{
+        price = _price;
+    } 
+
+    function getPrice() public view returns(uint){
+        return(price);
+    }
+
+    function createURL(address newURL) internal {
+        yourUrl = newURL;
+        ZonasProvs[msg.sender] = newURL;
+    }
+    
+    function mint() public payable returns(bool){
+        require(msg.value == price, "Price is higher");
+        //require(balance[msg.sender] < 1, "Max Reached");
+        supply = supply++;
+        balance[msg.sender]++;
+        createURL(msg.sender);
+        balanceETH = balanceETH + msg.value;
+        return(isAlphaProv = true);
+    }
+
+    function getURL() public view returns (address) {
+        return (ZonasProvs[msg.sender]);
+    }
+
+    function seeIfIsAlphaProv() public view returns(bool){
+        return(isAlphaProv);
+    }
+
+    function withdrawETH() public onlyOwner {
+        uint balanceOfETH = address(this).balance;
+        payable(msg.sender).transfer(balanceOfETH);
+        balanceETH = balanceETH - balanceOfETH;
+    }
+}
+
+//____________________________________________________________________
+
+contract factoryAlphaProvTradSig {
+
+    bool hasCreateOne;
+    mapping(address => address) public ContractsCreated;
+
+    function createCustomTradSig() public{
+        
+        CustomAlphaProvTradingSig newCustomTradSigAlphaProv = new CustomAlphaProvTradingSig();
+        ContractsCreated[msg.sender] = address(newCustomTradSigAlphaProv);
+        hasCreateOne = true;
+    }
+
+    function getContractCreated() public view returns(address){
+        return (ContractsCreated[msg.sender]);
+    }
+
+}
+
+//______________________________________________________________________
+
+contract CustomAccessPayment{ //INCOMPLETO
+        address owner;
+
+    constructor () {
         owner = msg.sender;
     }
 
@@ -34,65 +115,26 @@ contract AlphaProviderAccess is ERC721{
         balance[msg.sender]++;
         return(isAlphaProv = true);
     }
-
-    function seeIfIsAlphaProv() public view returns(bool){
-        return(isAlphaProv);
-    }
 }
 
-//____________________________________________________________________
-
-contract factoryAlphaProvTradSig is AlphaProviderAccess {
-
-    bool hasCreateOne;
-    mapping(address => address) public ContractsCreated;
-
-    function createCustomTradSig() public{
-        require(isAlphaProv == true, "you are not AlphaProvider, please get access NFT");
-        require(hasCreateOne == false, "You have already created");
-        CustomAlphaProvTradingSig newCustomTradSigAlphaProv = new CustomAlphaProvTradingSig();
-        ContractsCreated[msg.sender] = address(newCustomTradSigAlphaProv);
-        hasCreateOne = true;
-
-    }
-
-}
-
+//_____________________________________________________________________
 
 contract CustomAlphaProvTradingSig is AlphaProviderAccess {
-    //NOTAS: AÑADIR FUNCION DAR ACCESO A OTROS
+    //NOTAS: AÑADIR FUNCION DAR ACCESO A OTROS PARA POSTEAR
 
-    bool public funcCalled = false;
+    mapping(address => string) public messages;
 
-    function post() public returns(bool){
-        require(isAlphaProv == true, "Not Alpha Provider, please get access NFT");
-        funcCalled = true;
-        return(funcCalled);
-    } 
+    function post(string memory postMsg) public{
+        require(isAlphaProv == true, "you are not AlphaProvider, please get access NFT");
+        messages[msg.sender] = postMsg;
+
+    }
+
+    function readMsg() public view returns(string memory) {
+        return(messages[msg.sender]);
+    }
+
+
 }
 
 
-/*______________DISEÑO_____________________
-
-contract AccessAlphaProv{}
-
-contract CustomAccessNFT {}
-
-contract CustomSignals {}
-
-contract Factory {
-
-    new CustomAccesNFT() //Para el acceso de los usuarios
-        mapping msg.sender => address(newCustomAccessNFT) //Sender es el AlphaProv y se le relaciona con el contrato creado
-            js: if (addressConnected has CustomAccessNFT) {Access ok}
-
-
-    new CustomSignals()  
-        mapping msg.sender => address(newCustomSignals) //Para que el AlphaProv postee 
-                                                            y para que los users vean info
-}
-
-
-
-
-*/
